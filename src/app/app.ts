06d2fbc4d913df670179215e3bd707d31cbe6b1e
milicaapavlovic54 <milicaa.pavlovic54@gmail.com>
2026-03-06 17:27:37 +0100

    Add angular material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [
+    RouterOutlet, 
+    RouterLink, 
+    MatButtonModule, 
+    MatMenuModule,
+    MatToolbarModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
